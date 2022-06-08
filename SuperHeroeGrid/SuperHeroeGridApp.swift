6706ec85148bae8175dc9da7ffe962c6e9f9ec71
aleniten <joseherrero85@icloud.com>
2022-06-08 13:10:14 -0400

    Initial Commit

---
+++ SuperHeroeGrid/SuperHeroeGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroeGridApp.swift
+//  SuperHeroeGrid
+//
+//  Created by Jose Alejandro Herrero on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroeGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
